8f63b41fdcb6cc5009589429747aa1b9fa53ea49
franzannakarolina <franz.karols@gmail.com>
2022-12-02 23:40:15 -0300

    An animation library that I decided to test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-function App() {
+export default function App() {
+  const [animationParent] = useAutoAnimate({
+    duration: 100
+  });
+  const [items, setItems] = useState(["comprar leite"]);
+
+  const onRemoveItem = (itemToBeRemoved) => {
+    setItems((prev) => prev.filter((i) => i !== itemToBeRemoved));
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+
+    setItems((prev) => [...prev, ev.target.newItem.value]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Digite algo e dê enter" />
+      </form>
+      <ul ref={animationParent}>
+        {items.map((item) => (
+          <li key={item}>
+            {item}
+            <button onClick={() => onRemoveItem(item)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
